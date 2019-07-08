bee444aebdea9029a272a440b6857b50edb70495
Mr.Œżæn IT <52619403+faradisa823@users.noreply.github.com>
2019-07-09 02:35:42 +0700

    Cogan.py
    I love you Amanda Shelfi anastasya

---
+++ Cogan.py
@@ -0,0 +1,447 @@
+#!/system/xbin/bash
+# Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+###################################################
+# CTRL C-
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, 
+Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My 
+Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Rabbit Cyber Team Was 
+Here ... "
+read enter
+exit
+}
+=================================================
+echo $red"kontak admin: kontol"
+echo $green"Author : Mr.Œżæn "
+echo $blue"Team : Cyber Cirebon Salafy"
+echo $purple"Thanks To : MR.Noob"
+echo $cyan"Thanks To: Santri_cirebon"
+echo $white"Thanks To: WEB LENTERA"
+echo $white"ytb channel: WEB LENTERA"
+=================================================
+echo  $yellow" 01) Red Hawk"
+echo  $yellow" 02) D-Tect"
+echo  $yellow" 03) Top Up Ilegal ML"
+echo  $yellow" 04) Top Up Ilegal FF"
+echo  $yellow" 05) Webdav"
+echo  $yellow" 06) Metasploit"
+echo  $yellow" 07) Pulsa Gratis"
+echo  $yellow" 08) Ubuntu"
+echo  $yellow" 09) Youtube Dl"
+echo  $yellow" 10) viSQL "
+echo  $yellow" 11) multi-brute-webdav "
+echo  $yellow" 12) Bajinganv6 "
+echo  $yellow" 13) Tools RCT"
+echo  $yellow" 14) cc fresh Amazon "
+echo  $yellow" 15) install tools Mr.Fe3LL"
+echo  $yellow" 16) install tools InYourS3RV3R"
+echo  $yellow" 17) install tools MR.TR3V! ON"
+echo  $yellow" 18)  install tools TUANB4DUT"
+echo  $yellow" 19)  install tools TUAN N1CKN4M3"
+echo  $yellow" 20)  install sqlmap"
+echo  $yellow" 21) buat script deface"
+echo  $yellow" 22) install tools intect"
+echo  $yellow" 23) install tools Matematika "
+echo  $yellow" 24) install tools CYB3R GH0ST"
+echo $yellow" 25) install brute-webdav"
+echo  $red" 0) Exit "
+echo  $white""
+read -p "[Mr.CL4Y]> " bro;
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+echo  $red" Installing Red Hawk "
+sleep 1
+apt update && apt upgrade
+apt install php
+apt install git
+git clone 
+https://github.com/Tuhinshubhra/RED_HAWK
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+then
+clear
+echo  $red" Installing D-Tect "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git
+apt-get install python2
+git clone 
+https://github.com/shawarkhanethicalhacker/D-TECT
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+clear
+echo  $red" TOP UP ILEGAL ML "
+sleep 1
+echo  $white " TERTIPU KAMU BANGSAT "
+echo  $red" T E R T I P U "
+exit
+fi
+
+if [ $bro = 4 ] || [ $bro = 04 ]
+then
+clear
+echo  $red" TOP UP ILEGAL FF "
+sleep 1
+echo  #white" TERTIPU KAMU BANGSAT "
+echo  $red" T E R T I P U "
+exit
+fi
+
+if [ $bro = 5 ] || [ $bro = 05 ]
+then
+clear
+echo  $red" Installing Webdav "
+sleep 1
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna 
+requests
+apt install openssl curl
+pkg install libcurl
+mkdir webdav
+cd ~/webdav
+wget https://pastebin.com/raw/HnVyQPtR -O 
+webdav.py
+chmod 777 webdav.py
+cd ~/
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 06 ] || [ $bro = 6 ]
+then
+clear
+echo  $red" Installing Metasploit "
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install wget
+wget
+https://raw.githubusercontent.com/verluchie/termux-metasploit/master/install.sh
+chmod 777 install.sh
+sh install.sh
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 07 ] || [ $bro = 7 ]
+then
+clear
+echo  $red" PULSA GRATIS "
+sleep 1
+echo  $white" KERJA LAH GOBLOK "
+echo  $red" T E R T I P U "
+exit
+fi
+
+if [ $bro = 08 ] || [ $bro = 8 ]
+then
+clear
+echo  $red" Installing Ubuntu "
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install wget
+apt install proot
+git clone 
+https://github.com/Neo-Oli/termux-ubuntu.git
+cd ~/termux-ubuntu
+chmod +x ubuntu.sh
+sh ubuntu.sh
+echo " Fix network please wait "
+sleep 1
+echo "nameserver 8.8.8.8" >/data/data/com.termux/files/home/termux-ubuntu/ubuntu-fs/etc/$
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 09 ] || [ $bro = 9 ]
+then
+clear
+echo  $red" Installing Youtube DL "
+sleep 1
+apt update && apt upgrade
+apt install python
+pip3 install mps_youtube
+pip3 install youtube_dl
+apt install mpv
+echo " Untuk menjalankannya ketik "mpsyt" 
+tanpa tanda petik "
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+echo  $red" Installing viSQL "
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone 
+https://github.com/blackvkng/viSQL.git
+cd ~/viSQL
+chmod 777 viSQL.py
+cd ~/
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+echo  $red" Installing Multi-Brute-Webdav "
+sleep 1
+apt update && apt upgrade
+pkg install toilet
+pkg install sh
+pkg install git
+git clone https://github.com/INYOURS3RV3R/Multi-Brute-Webdav
+cd ~/Multi-Brute-Webdav
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ] 
+then
+clear
+echo  $red"installing bajinganv6"
+sleep 1
+pkg install php
+pkg install figlet
+pkg install python2
+pip2 install requests
+pip2 install termcolor
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd ~/BAJINGANv6
+echo  $red" T E R I N S T A L L "
+exit 
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+echo  $red"installing tools RCT"
+sleep 1
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/RabCT/rct
+cd ~/rct
+chmod +x rct
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+echo  $red" TOBAT OI JANGAN CARDING"
+sleep 1
+echo  $white" KASIAN ORTU LHU "
+echo  $red" T E R T I P U "
+exit
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+echo  $red"installing tools Mr.F3eLL"
+sleep 1
+apt update && apt upgrade
+pkg install php
+pkg install python2
+pkg install toilet
+pkg install git
+git clone https://github.com/Rafell/BHI
+cd ~/BHI
+chmod +x BHI
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+echo  $red"installing Tools InYourS3RV3R"
+sleep 1
+git clone https://github.com/INYOURS3RV3R/TOOLSINSTALLERv2
+cd ~/TOOLSINSTALLERv2
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+echo $red"installing Tools MR.TR3V!0N"
+sleep 1
+apt update && apt upgrade
+pkg install figlet
+pkg install lolcat
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install unzip
+pkg install python
+pkg install python2
+pkg install git
+git clone https://github.com/Rusmana-ID/rus
+cd ~/rus
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then 
+clear
+Echo $red"installing tools TUANB4DUT"
+sleep 1
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd ~/TOOLSINSTALLERv4
+chmod 777 TUANB4DUT..sh
+echo  $red" T E R I N S T A L L "
+exit
+fi
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+echo $red"installing tools TUANN1CKN4M3"
+sleep 1
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TU4NN1CKN4M3/TOOLSv2
+cd ~/TOOLSv2
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro =20 ] || [ $bro =20 ]
+then
+clear
+echo $red"installing sqlmap"
+sleep 1
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd ~/sqlmap
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro =21 ] || [ $bro = 21 ]
+then
+clear
+echo $red"installing script deface"
+sleep 1
+git clone https://github.com/Ubaii/script-deface-creator
+cd ~/script-deface-creator
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+echo $red"installing tools intect"
+sleep 1
+git clone https://github.com/intect15/inttuls
+cd ~/inttuls
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+echo $red"installing tools Matematika"
+sleep 1
+git clone https://github.com/hatakecnk/matematika.git
+cd ~/matematika
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro =24 ] || [ $bro = 24 ]
+then
+clear
+echo $red"installing tools Cyb3r Gh0st"
+sleep 1
+pkg update && pkg upgrade
+pkg install figlet
+pkg install php
+pkg install python python2 vim figlet curl
+pip2 install lolcat
+pkg install git
+git clone
+https://github.com/cyberghost69/bima-bagus-jabang-v3
+cd ~/ bima-bagus-jabang-v3
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro =25 ] || [ $bro = 25 ]
+then
+clear
+echo $red"installing tools brute-webdav"
+sleep 1
+pkg update && pkg upgrade
+pkg install curl
+pkg install figlet
+pkg install python2
+pip2 install lolcat
+pkg install git
+git clone
+https://github.com/TU4NN1CKN4M3/Brute-webdav
+cd ~/Brute-webdav
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo " Thanks To Rabbit Cyber Team "
+sleep 1
+echo " Mr.Œżæn Was here"
+sleep 1
+echo " love You "
+sleep 1
+echo " MDFK "
+sleep 1
+echo " love "
+sleep 1
+echo " Script Kiddie "
+sleep 1
+exit
+fi
